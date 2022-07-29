998505fb5c117bc4dd89913f12402f5e5a29f696
Naokiakazawa <naoki9akazawa@gmail.com>
2022-07-29 13:10:30 +0900

    edit rules

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,13 @@
-import { HardhatUserConfig } from 'hardhat/config'
-import '@nomicfoundation/hardhat-toolbox'
-import 'hardhat-gas-reporter'
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.9',
-  paths: { cache: 'hardhat-cache' },
+  solidity: "0.8.9",
+  paths: { cache: "hardhat-cache" },
   gasReporter: {
-    enabled: (process.env.GAS_REPORT) ? true : false
-  }
-}
+    enabled: process.env.GAS_REPORT ? true : false,
+  },
+};
 
-export default config
+export default config;
