613f1444d94013342b52c1cf9eddf02d0feaedaf
Naokiakazawa <naoki9akazawa@gmail.com>
2023-04-15 17:29:33 +0900

    add more options

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,27 +2,52 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-gas-reporter";
 
+const mnemonic: string =
+  process.env.MNEMONIC !== undefined
+    ? process.env.MNEMONIC
+    : "test test test test test test test test test test test junk";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  defaultNetwork: "hardhat",
   networks: {
-    hardhat: {},
+    hardhat: {
+      blockGasLimit: 10000000,
+    },
     localhost: {
       url: "http://127.0.0.1:8545",
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: {
+        mnemonic: mnemonic,
+      },
     },
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
       accounts: {
-        mnemonic:
-          process.env.MNEMONIC !== undefined
-            ? process.env.MNEMONIC
-            : "test test test test test test test test test test test junk",
+        mnemonic: mnemonic,
       },
-      //accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    matic: {
+      url: `https://rpc-mainnet.maticvigil.com/${process.env.INFURA_KEY}`,
+      accounts: {
+        mnemonic: mnemonic,
+      },
+    },
+    mumbai: {
+      url: `https://rpc-mumbai.maticvigil.com${process.env.INFURA_KEY}`,
+      accounts: {
+        mnemonic: mnemonic,
+      },
     },
   },
   paths: { cache: "hardhat-cache" },
   gasReporter: {
     enabled: process.env.GAS_REPORT ? true : false,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY !== undefined ? process.env.ETHERSCAN_KEY : "",
   },
 };
 
