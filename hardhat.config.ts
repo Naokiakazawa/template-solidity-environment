d6e95a324dad13d8030c67357ca5c170bf6ed163
Naokiakazawa <naoki9akazawa@gmail.com>
2023-05-18 20:35:14 +0900

    Update solidity version to 0.8.20

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,7 @@ const mnemonic: string =
     : "test test test test test test test test test test test junk";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
